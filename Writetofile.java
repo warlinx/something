1829903a43d46f468b3061b25cde05452bded078
warlinux <warlinux@yandex.ru>
2021-01-29 00:39:57 +0300

    Signed-off-by: warlinux <warlinux@yandex.ru>

---
+++ Writetofile.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Writetofile {
+    public static void main(String[] args) throws FileNotFoundException {
+        //Object test;
+        File file = new File("test.txt");
+        PrintWriter pw = new PrintWriter(file);
+        pw.println("test");
+        pw.println("test");
+        pw.close();
+    }
+}
+
+
+
