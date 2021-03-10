b6eba1e01984de1295fe54b2910a06736f0695e7
warlinux <warlinux@yandex.ru>
2021-03-10 21:28:34 +0300

    php

---
+++ src/com/company/diff/PHP.java
@@ -0,0 +1,14 @@
+package com.company.diff;
+
+public class PHP {
+    public static void echo(int s){
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        int $a = 12;
+        int $b = 13;
+        int $sum = $a + $b;
+        echo($sum);
+    }
+}
