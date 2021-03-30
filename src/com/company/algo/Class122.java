831bf8f1e03f7a8b6eb24d7709d1e9cd2cd04715
warlinux <warlinux@yandex.ru>
2021-03-30 23:50:32 +0300

    math

---
+++ src/com/company/algo/Class122.java
@@ -0,0 +1,16 @@
+package com.company.algo;
+
+public class Class122 {
+
+
+    public static void main(String[] args) {
+        double a =  Math.sin(0);
+        double b = Math.cos(0);
+        if ((a*a + b*b) == 1) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("Fuck You");
+        }
+    }
+}
---
+++ src/com/company/algo/vari.java
@@ -0,0 +1,10 @@
+package com.company.algo;
+
+public class vari {
+    public static void main(String[] args) {
+        int a = 0;
+        int b;
+        int t = a ; b = t ;
+        int а = b;
+    }
+}
