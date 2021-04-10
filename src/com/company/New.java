45eac358741c6a26adefeaf41f446736206044d5
warlinux <warlinux@yandex.ru>
2021-04-10 13:14:27 +0300

    added1

--- src/com/company/New.java
+++
@@ -1,8 +0,0 @@
- public class New {
-    public static void main(String[] args) {
-
-        System.out.println(1);
-
-    }
-    //System.out.println("The size of map is " + myHashMap.size());
-}
--- src/com/company/diff/Writetofile.java
+++ src/com/company/diff/Writetofile.java
@@ -1,3 +1,5 @@
+package com.company.diff;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
