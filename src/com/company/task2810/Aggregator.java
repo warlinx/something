7313a13837314cdcd7ca22aa30f952ba8bb8f42b
warlinux <warlinux@yandex.ru>
2021-04-10 13:12:05 +0300

    added

--- src/com/company/task2810/Aggregator.java
+++ src/com/company/task2810/Aggregator.java
@@ -1,5 +1,20 @@
 package com.company.task2810;
 
+import com.company.task2810.model.HHStrategy;
+import com.company.task2810.model.Model;
+import com.company.task2810.model.HabrCareerStrategy;
+import com.company.task2810.model.Provider;
+import com.company.task2810.view.HtmlView;
+
 public class Aggregator {
 
+    public static void main(String[] args) {
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, new Provider(new HHStrategy()), new Provider(new HabrCareerStrategy()));
+        Controller controller = new Controller(model);
+
+        view.setController(controller);
+
+        view.userCitySelectEmulationMethod();
+    }
 }
--- src/com/company/task2810/Controller.java
+++ src/com/company/task2810/Controller.java
@@ -1,4 +1,16 @@
 package com.company.task2810;
+import com.company.task2810.model.Model;
 
 public class Controller {
+
+    private Model model;
+
+    public Controller(Model model) {
+        if (model == null) throw new IllegalArgumentException();
+        this.model = model;
+    }
+
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
+    }
 }
